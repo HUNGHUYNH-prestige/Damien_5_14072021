e351c8655fc73ad9fc55990a5dadde64e7bc7c08
Hung <hunghuynh22@gmail.com>
2021-07-24 16:23:53 +0200

    modif js product

--- frontend/product.js
+++ frontend/product.js
@@ -5,6 +5,36 @@ h1.classList.add('text-center');
 h1.classList.add('bg-warning');
 h1.innerHTML = 'Voici votre produit';
 
+// get the ID of the product
+// query string with ?
+// avec ou sans window : retour dans console identique
+
+const queryString = window.location.search;
+console.log('voici le :' + queryString);
+// queryString returns voici le :?id=5be9c8541c9d440000665243
+
+
+// utiliser la classe URLSearchParams
+// parametreUrl contient la partie variable de l'URL
+// parametreUrl contient les parametres de l'URL
+
+const parametreUrl = new URLSearchParams(queryString);
+console.log(parametreUrl);
+// la console liste toute les methods de URLSearchParams
+
+// use get() method of the URLSearchParams to get the first result in parameter
+// get('id') can try with get('name')
+
+const productId = parametreUrl.get('id');
+console.log('content :' + productId);
+// la console affiche l'id du produit
+
+
+
+
+
+
+
 
 
 // function fetch to get products data
@@ -35,7 +65,7 @@ getProduct('http://localhost:3000/api/teddies');
 // l'article est un doudou
 
 function getArticle(doudou){
-    console.log(doudou);
+    console.table(doudou);
     let ici = window.location;
     console.log('la page est : ' + ici);
     let search = window.location.search;
