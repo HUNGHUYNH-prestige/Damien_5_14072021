20320a6619107b642f89c226044fc199a5983da3
Hung <hunghuynh22@gmail.com>
2021-07-30 19:20:38 +0200

    panier js affichage

--- frontend/panier.js
+++ frontend/panier.js
@@ -6,78 +6,169 @@ let marchandise = JSON.parse(localStorage.getItem('marchandise'));
 // afficher le contenu du local storage
 console.log(marchandise);
 
-let titre = document.createElement('h1');
-titre.innerHTML = 'Vos choix sont ici';
-titre.classList.add('h1');
-
-// s'accrocher a un element HTML existant pour commencer
+// GET THE ID ELEMENT TO ATTACH THING
 let panier = document.getElementById('products-panier');
-panier.innerHTML = 'PAGE PANIER';
-panier.appendChild(titre);
+panier.innerHTML = 'ICI le PANIER';
 
-// PAGE PANIER
-// Vos choix sont ici
-// suivi de l'identite du produit en commencant par le nom
+// get the data from the local storage
 
-let recaptitulatif = document.createElement('article');
-recaptitulatif.classList.add('recaptitulatif-panier-css');
-panier.appendChild(recaptitulatif);
+function afficherLocalStorage(){
+    if (marchandise === null || marchandise.length === 0){
+
+        let attention = document.createElement('h3');
+        attention.classList.add('h3');
+        attention.innerHTML = 'ATTENTION : PANIER VIDE';
+        panier.appendChild(attention);
+
+        let avertissement = confirm('OK pour Home\nANNULER pour rester');
+        if (avertissement == true){
+            window.location.href = 'index.html';
+        }
+
+        let disabled = document.getElementById('fieldset');
+        //disabled.classList.add('disabled');
+        disabled.setAttribute('disabled','');
+
+    }
+    else {
+
+        let resume = document.createElement('div');
+        panier.appendChild(resume);
+        sommeDesArticlesDansLePanier();
+        resume.innerHTML = 'Panier avec un ';
+
+        viderLaListeDesArticles();
+
+        recapitulatifDesProduitsDansLePanier();
 
 
-// si le local storage represente par marchandise est null donc vide
-// alors afficher un message : le panier est vide
-if (marchandise === null){
-    console.log('panier vide : data null');
-    let vide = document.createElement('div');
-    vide.innerHTML = 'Le panier est vide';
-    recaptitulatif.appendChild(vide);
-    console.log(vide);
-}
-// si le local storage est rempli 
-// alors marchandise non null
-// marchandise est un array
-else{
-    console.log('panier rempli : data ok');    
 
-    for (unite = 0; unite < marchandise.length; unite++){
-
-        console.log(unite);
-        console.log(marchandise.length);
-        console.log(marchandise);
-        console.log(marchandise[unite].nom);
-
-        let contenu;
         
-        contenu =
-        `<article class="container bg-info">
-            <div class="panier-fiche">Fiche Produit</div>
-
-                <div class="row d-flex justify-content-around">
-                    <div class="panier-data">Nom du produit</div>
-                    <div class="panier-data bg-warning">${marchandise[unite].nom}</div>
-                </div>
-
-                <div class="row d-flex justify-content-around">
-                    <div class="panier-data">Prix du produit</div>
-                    <div class="panier-data bg-warning">${marchandise[unite].prix}</div>
-                </div>
-
-                <div class="row d-flex justify-content-around">
-                    <div class="panier-data">Quantite du produit</div>
-                    <div class="panier-data bg-warning">${marchandise[unite].quantite}</div>
-                </div>
-
-                <div class="row d-flex justify-content-around">
-                    <div class="row d-flex justify-content-around">Supprimer ce produit</div>
-                    <i class="icone fas fa-trash bg-danger"></i>
-                </div>
-
-        </article>`;
-
-        recaptitulatif.innerHTML = contenu;
 
     }
 
+}
+
+
+function sommeDesArticlesDansLePanier(){
+    //console.log('faire la somme des articles dans le panier');
+
+    let total = 0;
+    if (marchandise != null || marchandise != 0){
+        let vide = document.createElement('div');
+        for (nombre = 0 ; nombre < marchandise.length ; nombre++){
+
+            let quantite = marchandise[nombre].quantite;
+            total = total + quantite;
+
+            /*
+            console.log(marchandise.length);
+            console.log(marchandise[nombre]);
+            console.log(marchandise[nombre].quantite);
+            console.log('total : ' + total);
+            */
+            
+        }
+        
+        vide.innerHTML = 'Total des articles = ' + total;
+        panier.appendChild(vide);
+        //return total;
+    }
+
+}
+
+
+
+function viderLaListeDesArticles(){
+    let vider = document.createElement('button');
+    vider.classList.add('btn');
+    vider.classList.add('btn-primary');
+    vider.classList.add('bouton-vider-liste');
+    vider.innerHTML = 'Vider la liste';
+    panier.appendChild(vider);
+
+    vider.addEventListener('click', viderTout);
+
+}
+
+function viderTout(propre){
+    propre.preventDefault();
+    alert('ATTENTION : vider le panier ! et direction HOME');
+    localStorage.removeItem('marchandise');
+    window.location.href = 'index.html';
+
+}
+
+function recapitulatifDesProduitsDansLePanier(){
+    console.log('recapitulatif des produits dans le panier');
+
+    for (unite = 0 ; unite < marchandise.length ; unite++){
+        //console.log(unite);
+        //console.log(marchandise.length);
+        //console.log(marchandise[unite]);
+
+        
+        /*
+        let article = marchandise[unite];
+        console.log(article);
+        console.log(marchandise[unite]);
+        */
+
+        let boite = document.createElement('div');
+        boite.classList.add('row');
+        boite.classList.add('d-flex');
+        boite.classList.add('boite');
+        panier.appendChild(boite);
+        
+
+        let nom = document.createElement('div');
+        nom.classList.add('information');
+        //nom.innerHTML = 'Nom de l\'article : ' + marchandise[unite]['nom'];
+        boite.appendChild(nom);
+
+        let quantite = document.createElement('div');
+        quantite.classList.add('information');
+        //quantite.innerHTML = 'Nombre de cet article : ' + marchandise[unite]['quantite'];
+        boite.appendChild(quantite);
+
+        // jusqu'ici tout est ok
+        // reprendre sur le bouton + et -
+        // fonction total du panier problem de nan
+        
+
+        
+
+
+        
+    }
     
 }
 
+// fonction pour faire la somme totale des articles dans le panier : prix unitaire x nombre d'articles dans la panier
+
+
+function totalDuPanier(){
+    let total = 0;
+    
+    for (nombre = 0; nombre < marchandise.length ; nombre++){
+
+        let prix = marchandise[nombre].prix;
+        let quantite = marchandise[nombre].quantite;
+
+        let total = total + (prix * quantite);
+        
+        console.log(prix);
+        console.log(quantite);
+        console.log(total);
+    }
+    return total;
+    
+}
+
+totalDuPanier();
+
+
+
+// call the main function
+
+afficherLocalStorage();
