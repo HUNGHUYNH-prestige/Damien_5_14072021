58cc840476ae1388fa0e6dcf5073870d8c822f6e
Hung <hunghuynh22@gmail.com>
2021-08-05 02:50:11 +0200

    form et post request test de fonctionnement

---
+++ frontend/confirmation.js
@@ -0,0 +1,21 @@
+console.log('CONFIRMATION');
+// code pour la confirmation de la post request methods
+
+
+// get the data from the local storage
+// commande dans le panier
+let orderId = JSON.parse(localStorage.getItem(orderId));
+console.log(commande);
+// montant total de la commande
+let ligneTotal = JSON.parse(localStorage.getItem(ligneTotal));
+console.log(ligneTotal);
+
+
+// faire la mise en page de la page de confirmation
+// 1. contenu textuel pour le message de confirmation
+
+// 2. lien vers HOME PAGE pour finir
+
+// 3. essayer de revenir sur la page precedente pour voir ce qu'il reste
+
+// faire le plan de test
--- frontend/panier.js
+++ frontend/panier.js
@@ -331,3 +331,205 @@ function retirerUnArticle(article){
 // definir la ligne total qui est la somme de toutes les lignes de tous les articles dans le panier
 var ligneTotal = 0;
 afficherLocalStorage();
+
+
+
+
+
+// --- --- --- --- --- --- --- ---
+
+
+
+
+
+console.log('Formulaire');
+// code pour le formulaire
+
+
+// get all elements from the DOM
+
+let prenom  = document.getElementById('first-name');
+let nom     = document.getElementById('last-name');
+let adresse = document.getElementById('address');
+let code    = document.getElementById('zip-code');
+let ville   = document.getElementById('city');
+let email   = document.getElementById('email');
+
+let messagePrenom  = document.getElementById('first-name-message');
+let messageNom     = document.getElementById('last-name-message');
+let messageAdresse = document.getElementById('address-message');
+let messageCode    = document.getElementById('zip-code-message');
+let messageVille   = document.getElementById('city-message');
+let messageEmail   = document.getElementById('email-message');
+
+let submit  = document.getElementById('submit');
+
+let order;
+
+// define the regular expression for pattern check before submit
+
+let emailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+let zipcodeValid = /^(?:[0-8]\d|9[0-8])\d{3}$/;
+let saisieValid = /^[^-\s][a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/;
+
+// define the function to check the regular expression when input
+function validFormCheckingRegularExpression(){
+    prenom.addEventListener('change', function(){
+        validationDeLaSaisie(saisieValid, this, messagePrenom);
+    })
+    nom.addEventListener('change', function(){
+        validationDeLaSaisie(saisieValid, this, messageNom);
+    })
+    adresse.addEventListener('change', function(){
+        validationDeLaSaisie(saisieValid, this, messageAdresse);
+    })
+    code.addEventListener('change', function(){
+        validationDeLaSaisie(zipcodeValid, this, messageCode);
+    })
+    ville.addEventListener('change', function(){
+        validationDeLaSaisie(saisieValid, this, messageVille);
+    })
+    email.addEventListener('change', function(){
+        validationDeLaSaisie(emailValid, this, messageEmail);
+    })
+}
+
+validFormCheckingRegularExpression();
+
+
+// define the function : validationDeLaSaisie(param1, param2, param3)
+// check each input line before validation
+function validationDeLaSaisie(regularExpressionPattern, saisie, zoneMessage){
+    let testing = regularExpressionPattern.test(saisie.value);
+    console.log(saisie.value);
+    console.log(testing);
+    if (testing == false){
+        console.log('je suis dans false');
+        saisie.classList.add('border-warning');
+        zoneMessage.innerHTML = 'ATTENTION ! Saisie incorrecte !';
+    }
+    else if(testing == true){
+        console.log('je suis dans true');
+        saisie.classList.remove('border-warning');
+        saisie.classList.add('border-success');
+        zoneMessage.innerHTML = 'PARFAIT !';
+    }
+    return testing;
+}
+
+
+
+// submit the form
+submit.addEventListener('click', function(quoi){
+    console.log('submit : attention !');
+    // si le champ est vide lorsque click on submit
+    attentionChampVide(prenom,  messagePrenom);
+    attentionChampVide(nom,     messageNom);
+    attentionChampVide(adresse, messageAdresse);
+    attentionChampVide(code,    messageCode);
+    attentionChampVide(ville,   messageVille);
+    attentionChampVide(email,   messageEmail);
+
+    // check data input incorrect or empty or correct
+    
+    if (!prenom.value || !nom.value || !adresse.value || !code.value || !ville.value || !email.value || !saisieValid.test(prenom.value) || !saisieValid.test(nom.value) || !saisieValid.test(adresse.value) || !saisieValid.test(code.value) || !saisieValid.test(ville.value) || !saisieValid.test(email.value)){
+        console.log('ATTENTION : soit VIDE soit INCORRECTE !');
+        console.log('refresh the page to try again !');
+        quoi.preventDefault();
+    }
+    //else{
+        // after checking
+        console.log("data correct ready to send request post methods !");
+        // send array to server
+        // define the array : empty
+        let arrayProductList = [];
+        // define the object to send : customer order ; type : object
+        order = {
+            contact : {
+                firstName : prenom.value,
+                lastName  : nom.value,
+                address   : adresse.value,
+                city      : ville.value,
+                email     : email.value
+            },
+            products : arrayProductList
+        }
+        console.log(arrayProductList);
+        console.log(order);
+
+        // for all items in local storage
+        // remarque ce script vient a la suite d'un script qui a deja charge le local storage
+        // ce script s'execute sur une page avec un local storage rempli donc il est possible de recuperer la data
+        
+        for (let indice = 0 ; indice < marchandise.length ; indice++){
+            arrayProductList.push(marchandise[indice].identifiant);
+            //arrayProductList.push(marchandise[indice].nom);
+        }
+
+        // total panier
+        // le total du panier a mettre dans un array pour l'envoi
+        let totalPanierArray = [];
+        totalPanierArray.push(ligneTotal);
+        console.log(totalPanierArray);
+        localStorage.setItem('ligneTotal', JSON.stringify(totalPanierArray));
+        
+        postRequest("http://localhost:3000/api/teddies/order");
+    //}
+})
+
+
+// define the function attentionChampVide(input, messageInput)
+// this function check if the input is empty or not
+function attentionChampVide(saisie, messageSaisie){
+    if (saisie.validity.valueMissing){
+        console.log(saisie.validity.valueMissing);
+        messageSaisie.innerHTML = 'ATTENTION : VIDE ! Merci de remplir';
+        messageSaisie.classList.add('text-center');
+        messageSaisie.classList.add('bg-warning');
+    }
+}
+
+
+// request fetch post method
+function postRequest(url){
+    fetch(url, {
+        method : 'post',
+        headers : {
+            'Content-type' : 'application/json'
+        },
+        body : JSON.stringify(order)
+    })
+    .then(function(result){
+        if (result.ok){
+            return result.json();
+        }
+    })
+    .then(function(data){
+        console.log(data);
+
+        // get the orderId from the server
+        let orderId = data.orderId;
+        console.log(orderId);
+
+        // create array for orderId
+        let orderIdArray = [];
+        orderIdArray.push(orderId);
+        console.log(orderIdArray);
+
+        // local storage set item
+        localStorage.setItem('orderId', JSON.stringify(orderIdArray));
+
+        // go confirmation
+        window.location.href = 'confirmation.html';
+
+        // check console
+        console.log('Sending Post Request : OK !');
+    })
+    .catch(function(error){
+        error = 'ATTENTION ! Error Request Post !';
+        console.log(error);
+        alert('OH ! OH ! ALERT !');
+    })
+}
+
+console.log('fin de code');
